9a98927566d847f05b3ec30df43cf8dd0072f477
Valentyn Hutsaliuk <valentyn.hutsaliuk@dxc.com>
2022-07-29 15:47:45 +0300

    Application for displaying radio widget with validation has been created.

---
+++ lib/home_page.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:test_task_1/radio_form_field.dart';
+
+///Home Page of the application
+class HomePage extends StatelessWidget {
+  HomePage({Key? key}) : super(key: key);
+
+  final _formKey = GlobalKey<FormState>();
+
+  final _snackBar =
+      const SnackBar(content: Text("Validation successfully. Submitting..."));
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Form(
+          key: _formKey,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              //Here is the new Radio widget that allows validation
+              RadioFormField(
+                items: const [
+                  Text('Woman'),
+                  Text('Man'),
+                  Icon(Icons.woman),
+                  Icon(Icons.man)
+                ],
+                validatior: (value) =>
+                    value == null ? "You haven't selected any option!" : null,
+              ),
+              //The button that alows a user to start validating operation
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 8),
+                child: ElevatedButton(
+                  onPressed: () {
+                    if (_formKey.currentState?.validate() ?? false) {
+                      ScaffoldMessenger.of(context).showSnackBar(_snackBar);
+                    }
+                  },
+                  child: const Text("Submit"),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/radio_form_field.dart
@@ -0,0 +1,108 @@
+import 'package:flutter/material.dart';
+
+/// Field with radio tiles elements.
+/// That allows to made a validation.
+///
+/// Number of options and tiles title are depends on [items] property.
+/// The [validatior] property allows you set the rule for validation.
+///
+///Example:
+/// ```dart
+///RadioFormField(
+///  items: const [
+///    Text('Man'),
+///    Icon(Icons.woman),
+///  ],
+///  validatior: (value) =>
+///      value == null ? "You haven't selected any option!" : null,
+///),
+/// ```
+class RadioFormField extends StatefulWidget {
+  const RadioFormField({
+    Key? key,
+    required this.items,
+    required this.validatior,
+  })  : assert(items.length > 1),
+        super(key: key);
+
+  final List<Widget> items;
+  final String? Function(int? value) validatior;
+
+  @override
+  State<RadioFormField> createState() => _RadioFormFieldState();
+}
+
+class _RadioFormFieldState extends State<RadioFormField> {
+  int? _currentSelectedIndex;
+
+  final _cardBorderRadius = 15.0;
+  final _radioSplashRadius = 0.0;
+
+  @override
+  Widget build(BuildContext context) {
+    return FormField<int?>(
+      validator: widget.validatior,
+      builder: (field) => Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          ListView.builder(
+            shrinkWrap: true,
+            itemCount: widget.items.length,
+            itemBuilder: (context, index) => Card(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(_cardBorderRadius),
+              ),
+              clipBehavior: Clip.hardEdge,
+              child: ListTile(
+                title: Align(
+                  alignment: Alignment.centerLeft,
+                  child: widget.items[index],
+                ),
+                trailing: Radio<int>(
+                  splashRadius: _radioSplashRadius,
+                  value: index,
+                  groupValue: _currentSelectedIndex,
+                  onChanged: (value) {
+                    setState(() {
+                      field.didChange(value);
+                      _currentSelectedIndex = value;
+                    });
+                  },
+                ),
+                onTap: () {
+                  setState(() {
+                    field.didChange(index);
+                    _currentSelectedIndex = index;
+                  });
+                },
+              ),
+            ),
+          ),
+          field.hasError
+              ? ErrorTextWidget(errorText: field.errorText ?? '')
+              : Container(),
+        ],
+      ),
+    );
+  }
+}
+
+class ErrorTextWidget extends StatelessWidget {
+  const ErrorTextWidget({
+    Key? key,
+    required this.errorText,
+  }) : super(key: key);
+
+  final String errorText;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16),
+      child: Text(
+        errorText,
+        style: TextStyle(color: Theme.of(context).errorColor),
+      ),
+    );
+  }
+}
